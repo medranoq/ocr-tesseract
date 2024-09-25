6b75a09472196cc90517f0ef769745c26839a236
IE DEV 5 <iedev5@informaticaelectoral.com>
2024-09-25 12:42:41 -0700

    deteccion
    pruebas de deteccion con distintas tecnicas

--- app.py
+++ app.py
@@ -1,40 +1,70 @@
+from os import remove
+
 import cv2
 import numpy as np
+import pytesseract
+from pytesseract import Output
+
 
 # get grayscale image
 def get_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#thresholding
-def get_thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY)[1]
 
-#opening - erosion followed by dilation
+
+# thresholding
+def get_thresholding(image):
+    return cv2.threshold(image, 40, 200, cv2.THRESH_BINARY)[1]
+
+
+# opening - erosion followed by dilation
 def get_opening(image):
-    kernel = np.ones((5,5),np.uint8)
+    kernel = np.ones((5, 5), np.uint8)
     return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
 
-#canny edge detection
+
+# canny edge detection
 def get_canny(image):
     return cv2.Canny(image, 100, 200)
 
-im = cv2.imread('img/py_logo.png') #función para cargar la imágen
+
+def remove_noise(image):
+    return cv2.medianBlur(image, 7)
+
+
+# dilation
+def dilate(image):
+    kernel = np.ones((2, 2), np.uint8)
+    return cv2.dilate(image, kernel, iterations=1)
+
+
+# erosion
+def erode(image):
+    kernel = np.ones((5, 5), np.uint8)
+    return cv2.erode(image, kernel, iterations=4)
+
+
+# opening - erosion followed by dilation
+def opening(image):
+    kernel = np.ones((5, 5), np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+
+def save_img(filename, im):
+    cv2.imwrite(filename, im)
+
+
+def get_text(img):
+    return pytesseract.image_to_data(img, output_type=Output.DICT)
+
+
+im = cv2.imread('img/recorte.jpg')  # función para cargar la imágen
 
 im_gray = get_grayscale(im)
 
-im_thr_bin = get_thresholding(im_gray)
+noise = remove_noise(im_gray)
 
-im_op = get_opening(im_gray)
+save_img('erode_binary.jpg',get_thresholding(noise))
 
-im_canny = get_canny(im_gray)
+d = get_text(get_thresholding(noise))
 
-print(im.shape)
-print(im_gray.shape)
-print(im_thr_bin.shape)
-print(im_op.shape)
-
-#guardar imágen
-cv2.imwrite('img/py_gray_logo.png', im_gray)
-cv2.imwrite('img/py_thr_bin_logo.png',im_thr_bin)
-cv2.imwrite('img/py_open_logo.png',im_op)
-cv2.imwrite('img/py_canny_logo.png',im_canny)
-
+print(d['text'])
